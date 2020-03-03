8665104e6279326d3f59b8c8d531d90532ca1329
Alexis4810 <alexisfraile356@gmail.com>
2020-03-03 13:41:32 -0600

    ejercicio2

---
+++ portafolioCesarfrailee/ejercicio2/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.txtoctal = new System.Windows.Forms.TextBox();
+            this.txthexadecimal = new System.Windows.Forms.TextBox();
+            this.txtbinario = new System.Windows.Forms.TextBox();
+            this.txtdecimal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "decimal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "equivale a:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "binario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "octal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "hexadecimal";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(12, 223);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 5;
+            this.btncalcular.Text = "calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Location = new System.Drawing.Point(135, 222);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpiar.TabIndex = 6;
+            this.btnlimpiar.Text = "limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(256, 222);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 7;
+            this.btnsalir.Text = "salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // txtoctal
+            // 
+            this.txtoctal.Location = new System.Drawing.Point(110, 124);
+            this.txtoctal.Name = "txtoctal";
+            this.txtoctal.Size = new System.Drawing.Size(100, 20);
+            this.txtoctal.TabIndex = 8;
+            // 
+            // txthexadecimal
+            // 
+            this.txthexadecimal.Location = new System.Drawing.Point(110, 162);
+            this.txthexadecimal.Name = "txthexadecimal";
+            this.txthexadecimal.Size = new System.Drawing.Size(100, 20);
+            this.txthexadecimal.TabIndex = 9;
+            // 
+            // txtbinario
+            // 
+            this.txtbinario.Location = new System.Drawing.Point(110, 87);
+            this.txtbinario.Name = "txtbinario";
+            this.txtbinario.Size = new System.Drawing.Size(100, 20);
+            this.txtbinario.TabIndex = 10;
+            // 
+            // txtdecimal
+            // 
+            this.txtdecimal.Location = new System.Drawing.Point(110, 10);
+            this.txtdecimal.Name = "txtdecimal";
+            this.txtdecimal.Size = new System.Drawing.Size(100, 20);
+            this.txtdecimal.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 450);
+            this.Controls.Add(this.txtdecimal);
+            this.Controls.Add(this.txtbinario);
+            this.Controls.Add(this.txthexadecimal);
+            this.Controls.Add(this.txtoctal);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnlimpiar;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.TextBox txtoctal;
+        private System.Windows.Forms.TextBox txthexadecimal;
+        private System.Windows.Forms.TextBox txtbinario;
+        private System.Windows.Forms.TextBox txtdecimal;
+    }
+}
+
---
+++ portafolioCesarfrailee/ejercicio2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            int numeroDec = Convert.ToInt32(txtdecimal.Text);
+            txtbinario.Text = Convert.ToString(numeroDec, 2);
+            txtoctal.Text = Convert.ToString(numeroDec, 8);
+            txthexadecimal.Text = Convert.ToString(numeroDec, 16);
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            txtbinario.Clear();
+            txtdecimal.Clear();
+            txthexadecimal.Clear();
+            txtoctal.Clear();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    }
+}
