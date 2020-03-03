88f8c97541560fb6c837729615ea90ee3699742b
Alexis4810 <alexisfraile356@gmail.com>
2020-03-03 14:08:01 -0600

    ejercicio4

---
+++ portafolioCesarfrailee/ejercicio4/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbotitulo = new System.Windows.Forms.ComboBox();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.txtsubtotal = new System.Windows.Forms.TextBox();
+            this.txtimpuesto = new System.Windows.Forms.TextBox();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnnueva = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "titulo de libro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "precio:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "cantidad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "subtotal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "impuesto(13%)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "total a pagar";
+            // 
+            // cbotitulo
+            // 
+            this.cbotitulo.FormattingEnabled = true;
+            this.cbotitulo.Location = new System.Drawing.Point(114, 60);
+            this.cbotitulo.Name = "cbotitulo";
+            this.cbotitulo.Size = new System.Drawing.Size(205, 21);
+            this.cbotitulo.TabIndex = 6;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(114, 101);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(100, 20);
+            this.txtprecio.TabIndex = 7;
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Location = new System.Drawing.Point(114, 135);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtcantidad.TabIndex = 8;
+            // 
+            // txtsubtotal
+            // 
+            this.txtsubtotal.Location = new System.Drawing.Point(114, 175);
+            this.txtsubtotal.Name = "txtsubtotal";
+            this.txtsubtotal.Size = new System.Drawing.Size(100, 20);
+            this.txtsubtotal.TabIndex = 9;
+            // 
+            // txtimpuesto
+            // 
+            this.txtimpuesto.Location = new System.Drawing.Point(114, 219);
+            this.txtimpuesto.Name = "txtimpuesto";
+            this.txtimpuesto.Size = new System.Drawing.Size(100, 20);
+            this.txtimpuesto.TabIndex = 10;
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(114, 252);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(100, 20);
+            this.txttotal.TabIndex = 11;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(27, 297);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 12;
+            this.btncalcular.Text = "calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnnueva
+            // 
+            this.btnnueva.Location = new System.Drawing.Point(192, 297);
+            this.btnnueva.Name = "btnnueva";
+            this.btnnueva.Size = new System.Drawing.Size(75, 23);
+            this.btnnueva.TabIndex = 13;
+            this.btnnueva.Text = "nueva";
+            this.btnnueva.UseVisualStyleBackColor = true;
+            this.btnnueva.Click += new System.EventHandler(this.btnnueva_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(353, 297);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 14;
+            this.btnsalir.Text = "salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 450);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnnueva);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.txtimpuesto);
+            this.Controls.Add(this.txtsubtotal);
+            this.Controls.Add(this.txtcantidad);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.cbotitulo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbotitulo;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.TextBox txtsubtotal;
+        private System.Windows.Forms.TextBox txtimpuesto;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnnueva;
+        private System.Windows.Forms.Button btnsalir;
+    }
+}
+
---
+++ portafolioCesarfrailee/ejercicio4/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio4
+{
+    public partial class Form1 : Form
+    {
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double cantidad, impuesto, precio, subtotal, total;
+
+            cantidad = Convert.ToDouble(txtcantidad.Text);
+            precio = Convert.ToDouble(txtprecio.Text);
+            subtotal = cantidad * precio;
+            impuesto = subtotal * 0.13;
+            total = impuesto + subtotal;
+            txtsubtotal.Text = string.Format("{0:c2}", subtotal);
+            txtimpuesto.Text = string.Format("{0:c2}", impuesto);
+            txttotal.Text = string.Format("{0:c2}", total);
+
+        }
+
+        private void btnnueva_Click(object sender, EventArgs e)
+        {
+            txtcantidad.Clear();
+            txtimpuesto.Clear();
+            txtprecio.Clear();
+            txtsubtotal.Clear();
+            txttotal.Clear();
+            cbotitulo.ResetText();
+
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
